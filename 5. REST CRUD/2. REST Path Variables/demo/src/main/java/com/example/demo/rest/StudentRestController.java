8e7a15e2beb18c5ace8b6a322037dbb489023f93
Arijit Sarkar <arijitsrkr13@gmail.com>
2023-09-16 22:24:54 +0530

    update

--- 5. REST CRUD/2. REST Path Variables/demo/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 5. REST CRUD/2. REST Path Variables/demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +15,8 @@ public class StudentRestController {
 
     private List<Student> theStudents;
 
-    @PostConstruct   // Spring calls the methods annotated with @PostConstruct only once, just after the initialization of bean properties
+    @PostConstruct
+    // Spring calls the methods annotated with @PostConstruct only once, just after the initialization of bean properties
     public void loadData() {
 
         theStudents = new ArrayList<>();
@@ -28,7 +28,6 @@ public class StudentRestController {
     }
 
 
-
     @GetMapping("/students")
     public List<Student> getStudents() {
 
@@ -38,6 +37,53 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getSingleStudent(@PathVariable int studentId) { // The @PathVariable annotation is used to extract the value from the URL
 
+        // check the studentID against the list size
+        if ((studentId >= theStudents.size()) || studentId < 0) {
+            throw new StudentNotFoundException("Student ID Not Found"); // New Exception is thrown
+        }
         return theStudents.get(studentId);
     }
+
+    // Add an Exception Handler to handle the above thrown Exception
+    @ExceptionHandler
+    // The @ExceptionHandler is an annotation used to handle the specific exceptions and sending the custom responses to the client.
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) // StudentErrorResponse : Type of the Response Body | StudentNotFoundException : Exception Type to Handle
+    {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        // return a ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND); //  ResponseEntity represents the whole HTTP response: status code, headers, and body.
+
+    }
+
+    // Add Another Exception Handler to catch any type of Exception thrown
+    @ExceptionHandler
+    // The @ExceptionHandler is an annotation used to handle the specific exceptions and sending the custom responses to the client.
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) // StudentErrorResponse : Type of the Response Body | StudentNotFoundException : Exception Type to Handle
+    {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        // return a ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND); //  ResponseEntity represents the whole HTTP response: status code, headers, and body.
+
+    }
 }
+
+
+
+
+
