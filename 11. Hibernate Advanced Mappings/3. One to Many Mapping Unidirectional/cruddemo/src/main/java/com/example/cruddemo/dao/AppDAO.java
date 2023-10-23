eb5e053d779c17f600eeb43d791f3e740384b882
Arijit Sarkar <arijitsrkr13@gmail.com>
2023-10-23 10:31:54 +0530

    update

---
+++ 11. Hibernate Advanced Mappings/3. One to Many Mapping Unidirectional/cruddemo/src/main/java/com/example/cruddemo/dao/AppDAO.java
@@ -0,0 +1,37 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Course;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailsById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorWithCoursesUsingJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+
+
+
+}
